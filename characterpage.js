17cc1acda73c5e6c344ce86a07c95659e83b1859
Siann <siannhann@gmail.com>
2024-04-17 19:38:48 -0400

    what happeneddddddddddd boooo

--- characterpage.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const spriteImage = new Image();
-    spriteImage.src = 'PINKSPRITE.png';
-    spriteImage.onload = function() {
-        drawImage(spriteImage, 0, 0, 500px, 500px);
-    };
-}
