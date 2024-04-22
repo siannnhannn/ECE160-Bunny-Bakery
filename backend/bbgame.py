669ceccd6d138c0566771f77003b7e5046e86a88
Siann <siannhann@gmail.com>
2024-04-22 00:12:26 -0400

    ughhh

---
+++ backend/bbgame.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__) 
+
+@app.route('/')
+def bbgame():
+    return render_template('bbgame.html')
+
+@app_route('/static/js', methods=['POST'])
+def process():
+    data = request.get_json()
+    x_result = data['x']
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    import app
+    app.run(debug=True)
