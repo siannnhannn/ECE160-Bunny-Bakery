3aeab1703aeed45e03bc5a9958488f686cc751cd
Dylan Meyer-O'Connor <dylansmo2003@gmail.com>
2024-04-22 20:38:55 -0400

    moved login to main flask file, deleted flask_login

--- backend/bbgame.py
+++ backend/bbgame.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+
 
 app = Flask(__name__) 
+
 
 @app.route('/')
 def bbgame():
     return render_template('bbgame.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if username in users and users[username] == password:
+            return '<h1>Welcome!!!</h1>'
+        else:
+            return '<h1>invalid credentials!</h1>'
+    else:
+        return render_template('login.html')
 if __name__ == '__main__':
 #    import app
     app.run(debug=True)
